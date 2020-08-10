f97d6c978926f8b9f6a820a95637a1be24eb5240
roblescristian991 <roblescristian991@gmail.com>
2020-08-10 00:19:17 -0500

    add_edge() seg. fault

--- headers/base_graph.hpp
+++ headers/base_graph.hpp
@@ -35,7 +35,7 @@ namespace gph
 
     public:
         Edge(){}
-        Edge(const int& weight);
+        Edge(const int& weight, Vertex<T> *dst);
         ~Edge();
     };
 
@@ -80,21 +80,26 @@ namespace gph
         friend class Edge<T>;
 
         Vertex<T>* operator[](const int& index);
-        Vertex<T>* getVertex(const std::string& id);
+        Vertex<T>* get_vertex(const std::string& id);
+        Vertex<T>* get_vertex(const T& value);
 
     public:
         Graph(){}
         Graph(const std::string& id, bool directed = false, bool weighted = WEIGHTLESS);
         ~Graph();
 
-        const int& getVertices() const;
-        const int& getEdges() const;
-        const std::string& getId() const;
+        const int& get_vertices() const;
+        const int& get_edges() const;
+        const std::string& get_id() const;
+        const float& get_weight(const std::string& org, const std::string& dst) const;
 
-        void add_vertex(const std::string &id, const T &value);
-        void add_edge(const Vertex<T>*& org, const Vertex<T>*& dst, const int& weight);
         bool is_empty();
+        void add_vertex(const std::string &id, const T &value);
+        void add_edge(const std::string& org, const std::string& dst, const int& weight);
         bool vertex_exists(const std::string& id);
+        bool vertex_exists(const T& value);
+        bool edge_exists(const std::string& org, const std::string& dst);
+        void print(const Vertex<T>& vtx = nullptr, bool edges = false);
     };
 
     ///////////////////////////////////
@@ -102,11 +107,11 @@ namespace gph
     ///////////////////////////////////
 
     template <typename T>
-    Edge<T>::Edge(const int& weight)
+    Edge<T>::Edge(const int& weight, Vertex<T> *dst)
     {
-        this->dstVtx = nullptr;
+        this->dstVtx = dst;
         this->nextEdge = nullptr;
-        this->weight = weight;
+        this->weight = new float(weight);
     }
 
     template <typename T>
@@ -171,15 +176,15 @@ namespace gph
     }
 
     template <typename T>
-    const int& Graph<T>::getVertices() const
+    const int& Graph<T>::get_vertices() const
     { return this->vertices; }
 
     template <typename T>
-    const int& Graph<T>::getEdges() const
+    const int& Graph<T>::get_edges() const
     { return this->edges; }
 
     template <typename T>
-    const std::string& Graph<T>::getId() const
+    const std::string& Graph<T>::get_id() const
     { return this->id; }
 
     template <typename T>
@@ -210,9 +215,28 @@ namespace gph
     /// Creates a new edge which will connect two given existing vertices.
     /// It'll also be appended to the origin vertex's edges list.
     template <typename T>
-    void Graph<T>::add_edge(const Vertex<T> *&org, const Vertex<T> *&dst, const int &weight)
+    void Graph<T>::add_edge(const std::string& org, const std::string& dst, const int &weight)
     {
-
+        Vertex<T> *auxOrg;
+        Edge<T> *auxEdg;
+        if (is_empty())
+            throw std::range_error("The graph has no vertices, therefore is not possible to add an edge");
+        if (!vertex_exists(org) || !vertex_exists(dst))
+            throw std::range_error("The given vertex is not available");
+        //auxOrg = get_vertex(org);
+        // If origin vertex's edge list is empty we create a new edge as the first in the list
+        std::cout << "value: " << auxOrg->id << std::endl;
+        if (auxOrg->adjEdge == nullptr) {
+            std::cout << "ASLKAJSDSF" << std::endl;
+            auxOrg->adjEdge = new Edge<T>(weight, get_vertex(dst));
+        }
+        // Otherwise we iterate over origin vertex's edge list until we reach the end of the list so
+        // we can create a new Edge (similar to push_back)
+        // In both cases we use dst as edge destination vertex
+        else {
+            for (auxEdg = auxOrg->adjEdge; auxEdg->nextEdge != nullptr; auxEdg != auxEdg->nextEdge);
+            auxEdg->nextEdge = new Edge<T>(weight, get_vertex(dst));
+        }
     }
 
     /// Returns true if a given id matches with an existing vertex
@@ -225,6 +249,20 @@ namespace gph
             return false;
         for (; auxVtx != nullptr; auxVtx = auxVtx->nextVtx)
             if (*auxVtx->id == id)
+                return true;
+        return false;
+    }
+
+    /// Returns true if a given value matches with an existing vertex
+    /// and it returns false otherwise
+    template<typename T>
+    bool Graph<T>::vertex_exists(const T &value)
+    {
+        Vertex<T>* auxVtx = this->root;
+        if (is_empty())
+            return false;
+        for (; auxVtx != nullptr; auxVtx = auxVtx->nextVtx)
+            if (*auxVtx->value == value)
                 return true;
         return false;
     }
@@ -247,7 +285,7 @@ namespace gph
     /// Returns a vertex using an id so the method can iterate over every vtx in the list until a matching
     /// id is found. If no matching id is found then the method will return a nullptr.
     template<typename T>
-    Vertex<T> *Graph<T>::getVertex(const std::string &id)
+    Vertex<T> *Graph<T>::get_vertex(const std::string &id)
     {
         Vertex<T>* auxVtx = this->root;
         Vertex<T>* result = nullptr;
@@ -259,5 +297,47 @@ namespace gph
             if (*auxVtx->id == id)
                 result = auxVtx;
         return result;
+    }
+
+    /// Returns a pointer to a vertex so it can be easily accessed
+    template<typename T>
+    Vertex<T> *Graph<T>::get_vertex(const T &value)
+    {
+        Vertex<T>* auxVtx = this->root;
+        Vertex<T>* result = nullptr;
+        if (is_empty())
+            throw std::range_error("This graph has no vertices");
+        if (!vertex_exists(value))
+            throw std::range_error("The given value doesn't correspond to any vertex stored in this graph");
+        for (; auxVtx != nullptr; auxVtx = auxVtx->nextVtx)
+            if (*auxVtx->value = value)
+                result = auxVtx;
+        return result;
+    }
+
+    template<typename T>
+    const float &Graph<T>::get_weight(const std::string& org, const std::string& dst) const
+    {
+        if (!vertex_exists(org) || !vertex_exists(dst))
+            throw std::range_error("One of the given vertices doesn't exist");
+    }
+
+    template <typename T>
+    bool Graph<T>::edge_exists(const std::string& org, const std::string& dst)
+    {
+        Vertex<T> *auxVtx;
+        Edge<T> *auxEdg;
+        if (!vertex_exists(org) || !vertex_exists(dst)) {
+            std::cout << "One of the given vertices doesn't exist" << std::endl;
+            return false;
+        }
+        auxVtx = get_vertex(org);
+        auxEdg = auxVtx->adjEdge;
+        if (auxEdg == nullptr)
+            return false;
+        for (; auxEdg->nextEdge != nullptr; auxEdg = auxEdg->nextEdge);
+        if (*auxEdg->dstVtx->id == dst)
+            return true;
+        return false;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "base_graph.hpp"
+
 using namespace std;
 using namespace gph;
 
@@ -11,8 +12,12 @@ int main()
     g.add_vertex("1", 34);
     g.add_vertex("2", 35);
     g.add_vertex("4", 36);
+    g.add_edge("1", "2", 23);
     cout << g.vertex_exists("2") << endl
-         << g.vertex_exists("1") << endl;
+         << g.vertex_exists("1") << endl
+         << g.edge_exists("1", "2") << endl;
+
+
 
     return 0;
 }
